4f93a435fc0867d3622cfdfe7544d7dc14b6c375
Wonski <sk42884@st.amu.edu.pl>
2012-07-12 15:59:42 +0200

    Dodanie projektu joker

---
+++ Joker/src/org/me/joker/JokerActivity.java
@@ -0,0 +1,13 @@
+package org.me.joker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JokerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
