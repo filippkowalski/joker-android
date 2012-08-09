0921954729f91fbb77b44252a06bde9f51703fef
mdfk13 <mdfk13@localhost>
2012-08-09 13:58:39 +0200

    Odpicowane ulubione i dodany Joker SplashScreen

---
+++ Joker/src/org/me/joker/SplashActivity.java
@@ -0,0 +1,34 @@
+package org.me.joker;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+	private long splashDelay = 2500; //3 seconds
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        
+        TimerTask task = new TimerTask()
+        {
+
+			@Override
+			public void run() {
+				finish();
+				Intent mainIntent = new Intent().setClass(SplashActivity.this, JokerActivity.class);
+				startActivity(mainIntent);
+			}
+        	
+        };
+        
+        Timer timer = new Timer();
+        timer.schedule(task, splashDelay);
+    }
+}
