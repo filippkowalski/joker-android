b9ab494a06e84293bea260f50c510e83950adabd
mdfk13 <mdfk13@localhost>
2012-10-18 12:45:20 +0200

    menu wysylania znowu dziala

--- Joker/src/org/me/joker/SecondIntent.java
+++ Joker/src/org/me/joker/SecondIntent.java
@@ -3,6 +3,7 @@ package org.me.joker;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.gesture.Gesture;
 import android.gesture.GestureLibraries;
 import android.gesture.GestureLibrary;
@@ -14,9 +15,9 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -86,12 +87,39 @@ public class SecondIntent extends Activity implements OnGesturePerformedListener
             break;
         case R.id.item2: // przycisk drugi - wyslij kawal znajomemu
         	
-        	
-        	
-        	//tutaj musi byc cos co wysyla tekst przez maila
+        	DatabaseAdapter database = new DatabaseAdapter(catId, getApplicationContext());
+         	
+        	        	Intent i = new Intent(Intent.ACTION_SEND);
+        	        	i.setType("message/rfc822");
+        	        	i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"TwojZnajomy@gmail.com"});
+        	        	i.putExtra(Intent.EXTRA_SUBJECT, "Fajny kawał");
+        	        	i.putExtra(Intent.EXTRA_TEXT   , database.loadJoke(catId));
+        	        	try {
+        	        	    startActivity(Intent.createChooser(i, "Wysyłanie maila..."));
+        	        	} catch (android.content.ActivityNotFoundException ex) {
+        	        	    Toast.makeText(SecondIntent.this, "Nie masz żadnych klientów pocztowych do wykonania tej akcji...", Toast.LENGTH_SHORT).show();
+        	        	}
         	
             break;
-        case R.id.item3:
+            
+        case R.id.item3:  //zaproponuj nam kawał
+			
+    		Intent i1 = new Intent(Intent.ACTION_SEND);
+        	i1.setType("message/rfc822");
+        	i1.putExtra(Intent.EXTRA_EMAIL  , new String[]{"void.studio7@gmail.com"});
+        	i1.putExtra(Intent.EXTRA_SUBJECT, "Propozycja kawału");
+        	i1.putExtra(Intent.EXTRA_TEXT   , "Zaproponuj nam kawał - jeśli będzie dobry, uwzględnimy go w kolejnym updeacie, a Tobie damy o tym znać ;)");
+        	try {
+        	    startActivity(Intent.createChooser(i1, "Wysyłanie maila..."));
+        	} catch (android.content.ActivityNotFoundException ex) {
+        	    Toast.makeText(SecondIntent.this, "Nie masz żadnych klientów pocztowych do wykonania tej akcji...", Toast.LENGTH_SHORT).show();
+        	}
+        	
+    		
+    			break;
+            
+            
+        case R.id.item4:
  	        SecondIntent.this.finish();        // przycisk powrot
             break; 
             
